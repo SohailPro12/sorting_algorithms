d2bdfd894759c71e9571d3a4a7fb80f014a6655f
Sohail Charef <charefsohayl@gmail.com>
2023-11-18 01:17:45 -0800

    Selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - the selection sort algorithm
+ * @array: the unsorted array to sort
+ * @size: the size fo the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+
+	size_t i, j;
+	int small_idx, temp;
+
+    	for (i = 0; i < size - 1; i++)
+	{
+		small_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[small_idx])
+				small_idx = j;
+		}
+
+		if (small_idx != i)
+		{
+			temp = array[small_idx];
+			array[small_idx] = array[i];
+			array[i] = temp;
+			print_array((const int *)array, size);
+		}
+	}
+}
